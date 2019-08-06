de8f5dac6ccb203396365631cd1396c600047b70
dylanfisher <hi@dylanfisher.com>
2019-08-05 23:23:19 -0600

    Remove http auth and update font

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,2 @@
 class ApplicationController < ActionController::Base
-  before_action :http_authenticate
-
-  private
-
-    def http_authenticate
-      unless Rails.env.development?
-        authenticate_or_request_with_http_basic do |username, password|
-          username == Rails.application.credentials.dig(:http_auth_user) && password == Rails.application.credentials.dig(:http_auth_password).to_s
-        end
-      end
-    end
 end
