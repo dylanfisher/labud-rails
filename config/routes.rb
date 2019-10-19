bd82a0168ac137c91b50ea6fc72c9d37c6d1951c
dylanfisher <hi@dylanfisher.com>
2019-10-19 11:59:47 -0400

    Remove duplicate route

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  # TODO: sort these new admin routes
-  namespace :admin do
-    resources :brush_strokes
-  end
   root to: 'home_pages#show'
   get '/home', to: redirect('/')
 
